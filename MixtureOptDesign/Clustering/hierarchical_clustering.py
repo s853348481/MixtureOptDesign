276eff1b70a0310acc692839583491e556b45d59
Geoffrey Lumuen <piperjef@gmail.com>
2023-03-10 10:37:28 +0100

    function that replaces design points with cluster values

--- MixtureOptDesign/Clustering/hierarchical_clustering.py
+++ MixtureOptDesign/Clustering/hierarchical_clustering.py
@@ -16,9 +16,10 @@ def hierarchical_clustering(data: np.ndarray, k: int) -> np.ndarray:
 
     Returns
     -------
-    means : ndarray
-        A 3 x k x 1 ndarray representing the mean coordinates of the data points for each cluster.
-
+    labels : ndarray
+        A 1-dimensional ndarray representing the cluster labels of each data point.
+    clusters : ndarray
+        A k x d ndarray representing the mean coordinates of the data points for each cluster.
     """
 
     # Compute the pairwise distances between all pairs of data points
@@ -39,9 +40,41 @@ def hierarchical_clustering(data: np.ndarray, k: int) -> np.ndarray:
         mean_x = np.mean(cluster[:, 0])
         mean_y = np.mean(cluster[:, 1])
         mean_z = 1 - (mean_x + mean_y)
-        means.append(np.array([mean_x, mean_y, mean_z]).reshape(3, 1))
+        means.append(np.array([mean_x, mean_y, mean_z]))
 
     # Stack the mean coordinates of each cluster
-    means = np.stack(means, axis=1)
+    clusters = np.stack(means)
 
-    return means
+    return labels, clusters
+
+
+def replace_with_clusters(data: np.ndarray, labels: np.ndarray, clusters: np.ndarray) -> np.ndarray:
+    """
+    Replace the points in a data array with the corresponding cluster values based on the labels.
+
+    Parameters
+    ----------
+    data : ndarray
+        An n x d array representing n data points in d dimensions.
+    labels : ndarray
+        An n x 1 array representing the cluster labels of the data points.
+    clusters : ndarray
+        A k x 1 array representing the cluster values.
+
+    Returns
+    -------
+    new_data : ndarray
+        An n x d array representing the data points with cluster values.
+
+    """
+
+    # Create an empty array to store the new values
+    new_data = np.empty_like(data)
+
+    # Iterate through each point in the data array
+    for i in range(data.shape[0]):
+        # Replace the point with the corresponding cluster value based on the label
+        new_data[i, :] = clusters[labels[i]-1]
+
+    return new_data
+
