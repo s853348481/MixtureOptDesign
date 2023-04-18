35be6317d73ff8365209f22fd55c38e796302274
Geoffrey Lumuen <piperjef@gmail.com>
2023-04-18 14:57:01 +0200

    generate simplex lattice and initial design

--- MixtureOptDesign/__init__.py
+++ MixtureOptDesign/__init__.py
@@ -8,6 +8,9 @@ from .CoordinateExchange.coordinate_exchange import CoordinateExchangeIOptimal,C
 from .HaltonDraws.halton_draws import HaltonDraws
 
 from .HaltonDraws.qmc_halton_draws import QMCHaltonDraws
+from .vns.utils import generate_initial_design, generate_simplex_lattice_design
+
+from .vns.vns import neighborhood_func_1,neighborhood_func_2,unique_rows,vns
 
 
 
